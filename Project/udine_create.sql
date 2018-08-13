98486e69ee1292f899bc8af48b7eef06dd50cc9c
Alex Steege <asteege@comcast.net>
2018-08-13 02:36:58 -0400

    uDine mySQL Create Table script

---
+++ Project/udine_create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users ( UserID int(30) NOT NULL AUTO_INCREMENT, 
+FirstName varchar(50) NOT NULL, 
+LastName varchar(50) NOT NULL,
+Email varchar(50) NOT NULL,     
+Password varchar(255) NOT NULL,     
+College varchar(255) NOT NULL, PRIMARY KEY (UserID) );
